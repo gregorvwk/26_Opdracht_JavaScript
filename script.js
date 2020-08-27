7290dc91b740ebc6c7635dc07d2ae5a5a3bf5b33
gregorvwk <69797933+gregorvwk@users.noreply.github.com>
2020-08-27 10:21:07 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+// zet de functie om naar een arrow functie
+const ikRockArrowFunctions = () => console.log("Joe, ik rock de arrow functions!");
+  
+  ikRockArrowFunctions();
+
+  const fivePlusSeven = () => 5 + 7;
+
+  fivePlusSeven();
+
+  () => console.log(1 + 2);
+
+  let myFunction = (a = 3, b = 4) => console.log(a + b);
+
+  myFunction();
+
+  let func = c => console.log(c + 5);
+
+  func();
+
+  let createObject = () => ({greeting: "hoi"});
+
+  console.log(createObject());
